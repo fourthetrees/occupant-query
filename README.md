734c7cd6fe57c17b5ec2975442b8426e551da17b
forrest <fspmarshall@gmail.com>
2017-01-26 16:44:01 -1000

    updated docs & modified tmp file system

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # occupant-query
+
 A simple elm app for displaying auto-generated queries on web kiosks.
+
+## Features
+
+- Elm's strong type system and compile-time checks, allow for quick, smooth,
+and error-free deployment on even minimally powered devices.
+
+- This app continues to function normally if/when the hosting device loses internet connectivity;
+all data is temporarily moved to local storage until connectivity with the server is re-established.
+
+- A simple [flask](http://flask.pocoo.org/) server is included in the `server/` directory,
+which dynamically provides parameters to the elm app & recieves responses.
+
+## Usage
+
+To compile & launch:
+
+````
+~/occupant-query$ elm-make src/Main.elm --output=server/static/main.js
+Success! Compiled 1 module.                                         
+Successfully generated server/static/main.js
+~/occupant-query$ cd server/
+~/occupant-query/server$ export FLASK_APP=server.py
+~/occupant-query/server$ flask run
+ * Serving Flask app "server"
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+````
+
+An example query is available at `http://127.0.0.1:5000/queries/test_query`.
+The server auto-generates queries from JSON files in the `server/tmp/queries/` directory.
+Check out [`server/tmp/queries/test_query.json`](./server/tmp/queries/test_query.json)
+to see the config file which generates the above example.
--- server/server.py
+++ server/server.py
@@ -12,12 +12,12 @@ import json
 app = Flask(__name__)
 app.debug = True
 
-# Generic greeting page
+# Because its rude not to say hello.
 @app.route('/')
 def landing():
     return 'hello world'
 
-#
+# Primary data-logging route.
 @app.route('/log_responses/<query>', methods = ['GET','POST'])
 @cross_origin()
 def log_responses(query):
@@ -35,23 +35,24 @@ def log_responses(query):
                 mimetype='application/json')
         return rsp
 
+# Primary query-serving route.
 @app.route('/queries/<query>', methods = ['GET'])
 def queries(query):
-    qfile = "tmp/{}.json".format(query)
-    if not path.isfile(qfile):
+    qfile = 'tmp/queries/{}.json'.format(query)
+    if not path.isfile(qfile) or query == 'config':
         raise Exception('no query named {}.'.format(query))
     with open(qfile) as fp:
         seed = json.load(fp)
     return render_template('main.html',seed=seed)
 
-# Storage function
+# Storage function -- overwrite as appropriate.
 def store_values(query,data):
     print('Values Recieved For {}'.format(query))
     print(json.dumps(data,indent=2))
-    #with open('tmp/config.json') as fp:
+    #with open('tmp/config/psql_config.json') as fp:
     #    config = json.load(fp)['psql']
     #psql.push(query,data,config['dbname'],config['tblname'])
 
-# Placeholder deployment update function
+# Placeholder deployment update function -- feature not yet implimented.
 def get_deployment(deployment):
     return json.dumps({"test":"someTest"})
